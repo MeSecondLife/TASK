19b4f92324fc0edc830625780e482df4472e903b
MeSecondLife <kirill.suslov2015@mail.ru>
2020-10-24 22:08:20 +0300

    Тестовое задание

--- src/com/company/TerminalXML.java
+++ src/com/company/TerminalXML.java
@@ -8,7 +8,7 @@ class TerminalXML //Класс для работы с коммандами c XML
     ArrayList<Command> commands = new ArrayList<Command>();//Список комманд терминала
     Scanner input = new Scanner(System.in);
 
-    public TerminalXML(){
+    public TerminalXML() {
         commands.add(new HelpCommand());
         commands.add(new NewTask());
         commands.add(new ListCommand());
@@ -19,15 +19,22 @@ class TerminalXML //Класс для работы с коммандами c XML
     }
 
     public void commandsWorking() {
+        String keyWord;
+        while (true) {
+            keyWord = input.nextLine();
 
-        String keyWord = input.nextLine();
+            if (keyWord.equals("end")) {
+                System.exit(0);
+            }
 
-        for (int i = 0; i < commands.size(); i++) {
-            if (keyWord.equals(commands.get(i).name)) {
-                commands.get(i).action();
-                return;
+            for (int i = 0; i < commands.size(); i++) {
+                if (keyWord.equals(commands.get(i).name)) {
+                    keyWord = "";
+                    commands.get(i).action();
+                    break;
+                }
+
             }
         }
-        System.out.println("Некорректный код. Используйте комманду 'help'");
     }
 }
